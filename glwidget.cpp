7223ced6e4bbb68dd37f660163d654215cedc7f6
paswd <paswd@yandex.ru>
2017-12-23 17:31:27 +0300

    Created an empty OpenGL form

---
+++ glwidget.cpp
@@ -0,0 +1,19 @@
+#include "glwidget.h"
+
+GLWidget::GLWidget(QWidget *parent) :
+    QGLWidget(parent)
+{
+
+}
+
+void GLWidget::initializeGL() {
+    glClearColor(1, 1, 1, 1);
+}
+
+void GLWidget::paintGL() {
+    glClear(GL_COLOR_BUFFER_BIT);
+}
+
+void GLWidget::resizeGL(int w, int h) {
+
+}
